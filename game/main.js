60686126d2997e6927392f2e2574edb6ca5e4af1
armine-yepremyan <armine.yepremyan@yara.com>
2023-09-13 23:53:10 +0400

    VScode game

---
+++ game/main.js
@@ -0,0 +1,315 @@
+const defaultTraversalX = [0, 1, 2, 3];
+const defaultTraversalY = [0, 1, 2, 3];
+let head = document.getElementById("head");
+let scoreBar = document.getElementById("scoreBar");
+let scoreItem = document.getElementById("score");
+let container = document.getElementById("container");
+let currentGame = null;
+let mask = document.querySelectorAll(".mask, .gameover, #again");
+let board = document.getElementById("board");
+let restartBtn = document.getElementById("restart");
+let againBtn = document.getElementById("again");
+
+document.addEventListener("DOMContentLoaded", () => {
+  currentGame = new Game();
+  currentGame.init();
+})
+
+const newGame = () => {
+  currentGame.clear();
+  currentGame = null;
+  currentGame = new Game();
+  currentGame.init();
+}
+
+// againBtn.onclick = newGame;
+
+class Game {
+  static dir = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1]
+  ];
+  constructor() {
+    this.board = document.getElementById("board");
+    this.item = document.querySelectorAll(".item");
+    this.tile = new Array(4);
+    this.score = 0;
+  }
+
+  clear = () => {
+    location.reload();
+  }
+
+  init = () => {
+    let promise = new Promise(resolve => {
+      for (let i = 0; i < 4; ++i) {
+        this.tile[i] = new Array(4).fill(0);
+      }
+      resolve()
+    });
+    promise
+      .then(this.createNewTile(2))
+      .then(() => document.addEventListener("keydown", this.onKeyDown));
+  }
+
+  createNewTile = (num = 1) => {
+    for (let i = 0; i < num; ++i) {
+      let rand = Math.floor(Math.random() * 16);
+      let x = Math.floor(rand / 4);
+      let y = rand % 4;
+
+      while (this.tile[x][y]) {
+        rand = Math.floor(Math.random() * 16);
+        x = Math.floor(rand / 4);
+        y = rand % 4;
+      }
+
+      let value = (Math.floor(Math.random() * 2) + 1) * 2;
+      this.tile[x][y] = value;
+      this.addTile({ x, y, val: value}, 250, false, true);
+    }
+  }
+
+  addTile = (tile, delay, merged, created = false) => {
+    let { x, y, val } = tile;
+    let coordinate = getCoordinate(this.item[x * 4 + y]);
+    let newTile = null;
+
+    if (merged || created) {
+      newTile = document.createElement("div");
+      let className = merged ? "mergeTile" : "newTile";
+      newTile.classList.add(className);
+      newTile.classList.add("item");
+      newTile.dataset.value = val;
+      newTile.innerHTML = val;
+      newTile.style.left = coordinate.left + "px";
+      newTile.style.top = coordinate.top + "px";
+      container.append(newTile);
+      
+      newTile.getBoundingClientRect();
+      newTile.style.transform = "scale(1)";
+      newTile.style.visibility = "hidden";
+    }
+    setTimeout(() => {
+      this.item[x * 4 + y].innerHTML = val;
+      this.item[x * 4 + y].dataset.value = val;
+    }, delay)
+
+  }
+
+  findFinalPosition = (n, current) => {
+    let res = current;
+    while (current.x >= 0 && current.x <= 3 && current.y >= 0 && current.y <= 3) {
+      res = current;
+      current = { x: current.x + Game.dir[n][0], y: current.y + Game.dir[n][1] };
+
+      if (
+        current.x > 3 ||
+        current.x < 0 ||
+        current.y > 3 ||
+        current.y < 0 ||
+        this.tile[current.x][current.y]
+      ) {
+        break;
+      }
+    }
+    return {
+      x: res.x,
+      y: res.y,
+      next: { x: current.x, y: current.y }
+    }
+  }
+
+  move = (oldTile, newTile) => {
+    let oldIndex = oldTile.x * 4 + oldTile.y;
+    let newIndex = newTile.x * 4 + newTile.y;
+    let merged = oldTile.val != newTile.val;
+
+    let oldCoordinate = getCoordinate(this.item[oldIndex]);
+
+    let moveTile = document.createElement("div");
+    moveTile.classList.add("moveTile", "item");
+    moveTile.dataset.value = oldTile.val;
+    moveTile.innerHTML = oldTile.val;
+    moveTile.style.left = oldCoordinate.left + "px";
+    moveTile.style.top = oldCoordinate.top + "px";
+
+    container.append(moveTile);
+    this.item[oldIndex].innerHTML = "";
+    this.item[oldIndex].dataset.value = 0;
+
+    this.addTile(newTile, 100, merged);
+    let newCoordinate = getCoordinate(this.item[newIndex]);
+    moveTile.style.left = newCoordinate.left + "px";
+    moveTile.style.top = newCoordinate.top + "px";
+    moveTile.style.visibility = "hidden";
+
+  }
+
+  updateScore = (score) => {
+    this.score += score;
+    scoreItem.innerHTML = this.score;
+    let item = document.createElement("div");
+    item.className = "scoreAdd";
+    item.innerHTML = `${score}`;
+    item.style.top = 60 + "px";
+    item.style.right = 130 + "px";
+    container.append(item);
+
+    setTimeout(() => {
+      item.style.top = -100 + "px";
+      item.style.opacity = 0;
+    }, 0);
+
+    setTimeout(() => {
+      item.remove()
+    }, 2000);
+  }
+
+  async moveTile(n, traversal) {
+    let changed = false;
+    let score = 0;
+    if (Game.dir[n][0] == 1) traversal.x = traversal.x.reverse();
+    if (Game.dir[n][1] == 1) traversal.y = traversal.y.reverse();
+
+    let lastChangedItem = null;
+
+    traversal.x.forEach((i) => {
+      traversal.y.forEach((j) => {
+        let val = this.tile[i][j];
+
+        if (val != 0) {
+          let current = { x: i, y: j, val: val };
+          let finalPosition = this.findFinalPosition(n, current);
+          let next = finalPosition.next;
+          let newTile;
+
+          if (
+            next.x >= 0 &&
+            next.x < 4 &&
+            next.y >= 0 &&
+            next.y < 4 &&
+            val == this.tile[next.x][next.y] &&
+            (!lastChangedItem ||
+              next.y != lastChangedItem.x ||
+              next.y != lastChangedItem.y)
+          ) {
+            score += val * 2;
+            newTile = { x: next.x, y: next.y, val: val * 2 };
+            lastChangedItem = { x: next.x, y: next.y };
+          } else {
+            newTile = { x: finalPosition.x, y: finalPosition.y, val: val };
+          }
+
+          if (!changed && (newTile.x != i || newTile.y != j)) {
+            changed = true;
+          }
+
+          if (newTile.x == i && newTile.y == j) return;
+          this.tile[i][j] = 0;
+          this.tile[newTile.x][newTile.y] = newTile.val;
+
+          this.move(current, newTile);
+        }
+      });
+    });
+    if (score) this.updateScore(score);
+    return changed;
+  }
+
+  isNoEmptySpace = () => {
+    for (let row of this.tile) {
+      for (let i of row) {
+        if (i == 0) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+  canMove = () => {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (
+          (j < 3 && this.tile[i][j] == this.tile[i][j+1]) ||
+          (i < 3 && this.tile[i][j] == this.tile[i + 1][j])
+        ) {
+          return true;
+        }
+      }
+    }
+    return false
+  }
+
+  endGame = () => {
+    document.removeEventListener("keydown", this.onKeyDown);
+    mask.forEach((i) => {
+      i.hidden = false;
+    })
+  }
+
+  onKeyDown = (event) => {
+    let dir = 0;
+    event.preventDefault();
+    switch(event.key) {
+      case "ArrowUp":
+        dir = 0;
+        break;
+      case "ArrowRight":
+        dir = 1;
+        break;
+      case "ArrowDown":
+        dir = 2;  
+        break;
+      case "ArrowLeft":
+        dir = 3;
+        break;
+      default:
+        return;
+    }
+    let traversal = {
+      x: [...defaultTraversalX],
+      y: [...defaultTraversalY]
+    };
+    clearAnimatedItem();
+
+    let promise = currentGame.moveTile(dir, traversal);
+
+    promise.then(moved => {
+      if (moved && !currentGame.isNoEmptySpace()) {
+        currentGame.createNewTile();
+      }
+      if (currentGame.isNoEmptySpace() && !currentGame.canMove()) {
+        currentGame.endGame();
+      }
+    })
+  }
+}
+
+const getCoordinate = (item) => {
+  let containerCoordinate = container.getBoundingClientRect();
+  let coordinate = item.getBoundingClientRect();
+
+  return {
+    left: coordinate.left - containerCoordinate.left,
+    top: coordinate.top - containerCoordinate.top
+  }
+}
+
+const clearAnimatedItem = () => {
+  let temp = document.querySelectorAll(".newTile, .mergeTile, .moveTile");
+  temp.forEach(t => t.remove());
+
+}
+
+window.addEventListener('message', event => {
+  const message = event.data;
+  switch(message.type) {
+    case "restart": {
+      newGame()
+    }
+  }
+})
---
+++ src/extension.ts
@@ -0,0 +1,105 @@
+import * as vscode from 'vscode';
+import { getNonce } from './utils/nonce';
+
+// This method is called when the extension is activated
+export function activate(context: vscode.ExtensionContext) {
+	const provider = new GameProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(GameProvider.viewType, provider)
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('2048.restart', () => {
+			provider.restart();
+		})
+	);
+}
+
+class GameProvider implements vscode.WebviewViewProvider {
+
+	public static readonly viewType = '2048.view';
+
+	private view?: vscode.WebviewView;
+
+	constructor(
+		private readonly extensionUri: vscode.Uri,
+	) {}
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this.view = webviewView;
+
+		webviewView.webview.options = {
+			enableScripts: true,
+			localResourceRoots: [
+				vscode.Uri.joinPath(this.extensionUri, 'game')
+			]
+		};
+		webviewView.webview.html = this.getHtmlWebview(webviewView.webview);
+	}
+
+	private getHtmlWebview(webview: vscode.Webview) {
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, 'game', 'main.js'));
+		const styleGame = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, 'game', 'style.css'));
+
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+				<title>2048</title>
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+				<link rel="stylesheet" href="${styleGame}">
+			</head>
+		
+			<body>
+				<div id="container">
+					<div id="head">
+						<h1 class="title">2048</h1>
+						<div id="scroreBar">
+							<div id="score">0</div>
+						</div>        
+					</div>
+					<div id="board">
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="item"></div>
+						<div class="mask" hidden></div>
+						<div class="gameover" hidden>Game over!</div>
+					</div>
+				</div>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+		</html>`;
+	}
+
+	public async restart(): Promise<void> {
+		if (this.view) {
+			this.view.webview.html = this.getHtmlWebview(this.view.webview);
+		}
+	}
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
